81aa70da1187aa663c7d45280f066c8f47463ca6
nix- <nix-@users.noreply.github.com>
2021-04-26 00:31:08 +0200

    Description notes for the part of the main code (integration example)

--- main.c
+++ main.c
@@ -3,12 +3,26 @@
 
 int main()
 {
-	char cmd_buff[2000] = {0,};
+	char cmd_buff[64] = {0,};
 
+	// init indexes
 	init_donut(&donut);
+
+	// info - user example
+	printf("Type help for getting list of commands.\r\n");
+	printf("try the follow:\r\n");
+	printf("  1: info\r\n");
+	printf("  2: help\r\n");
+	printf("  3: test -t 88\r\n");
+	printf("  4: example -h\r\n");
+	printf("  4: clear\r\n");
 	
     while (1) {
+    	// receiving character
     	donut_receive_key(getchar()); 
+
+    	// process the commands till there are some in the donut-buff
+    	//   note: new command will be processed after \n is received and stored!
     	while (donut_pull_cmd(cmd_buff)) {
         	// printing the accumulated command
         	printf(GRN "%s" EOC "\r\n", cmd_buff);
